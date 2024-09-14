46e73d5c4ffe3c47a00ce50fd0fba9e6b142eb01
Samuel Adams <sama29571@gmail.com>
2024-09-14 10:58:31 +0000

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_chat_app/colors.dart';
+import 'package:social_media_chat_app/firebase_options.dart';
 import 'package:social_media_chat_app/screens/mobile_layout_screen.dart';
 import 'package:social_media_chat_app/screens/web_layout_screen.dart';
 import 'package:social_media_chat_app/utils/responsive_layout.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
