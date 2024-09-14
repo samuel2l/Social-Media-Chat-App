ac3d77f663d9a18509639f0b47f94e060ca95581
Samuel Adams <sama29571@gmail.com>
2024-09-14 16:41:08 +0000

    add auth controller

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:social_media_chat_app/features/auth/respository/auth_repository.dart';
+
+final authControllerProvider=Provider((ref) => AuthController(authRepository: ref.watch(authRepositoryProvider)));
+
+class AuthController {
+  final AuthRepository authRepository;
+
+  AuthController({required this.authRepository});
+
+  void signInWPhone(BuildContext context, String number) async {
+    authRepository.signInWPhone(context, number);
+  }
+}
--- lib/features/auth/respository/auth_repository.dart
+++ lib/features/auth/respository/auth_repository.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:social_media_chat_app/features/auth/screens/otp.dart';
 import 'package:social_media_chat_app/features/common/utils/utils.dart';
+
+final authRepositoryProvider=Provider((ref) => AuthRepository(auth: FirebaseAuth.instance, firestore: FirebaseFirestore.instance));
 
 class AuthRepository {
   final FirebaseAuth auth;
