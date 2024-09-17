8994197444bd3c650e648a6c5be4f406d84bf702
Samuel Adams <sama29571@gmail.com>
2024-09-17 21:33:33 +0000

    add online info

--- lib/screens/mobile_chat_screen.dart
+++ lib/screens/mobile_chat_screen.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:social_media_chat_app/features/auth/controller/auth_controller.dart';
+import 'package:social_media_chat_app/models/user_model.dart';
 import 'package:social_media_chat_app/utils/colors.dart';
 import 'package:social_media_chat_app/widgets/chat_list.dart';
 
-class MobileChatScreen extends StatelessWidget {
+class MobileChatScreen extends ConsumerWidget {
   static const String routeName = '/chat-screen';
 
-  final String name,uid;
+  final String name, uid;
 
   const MobileChatScreen({super.key, required this.name, required this.uid});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: appBarColor,
-        title: Text(
-           name,
+        title: StreamBuilder<UserModel>(
+          stream: ref.watch(authControllerProvider).checkUserOnline(uid),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return Column(
+              children: [
+                Text(name),
+                Text(
+                  snapshot.data!.isOnline ? 'online' : 'offline',
+                  style: const TextStyle(fontSize: 15),
+                )
+              ],
+            );
+          },
         ),
         centerTitle: false,
         actions: [
@@ -35,6 +54,7 @@ class MobileChatScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
+          Text(name),
           const Expanded(
             child: ChatList(),
           ),
