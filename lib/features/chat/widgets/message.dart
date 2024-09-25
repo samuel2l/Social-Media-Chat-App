1bb1e4dac297ea20847753867839d1ed0eb68be1
Samuel Adams <sama29571@gmail.com>
2024-09-25 14:59:41 +0000

    add permission handler

--- lib/features/chat/widgets/message.dart
+++ lib/features/chat/widgets/message.dart
@@ -3,17 +3,53 @@ import 'package:flutter/material.dart';
 import 'package:social_media_chat_app/features/common/enums/message_type.dart';
 
 class Message extends StatelessWidget {
-  const Message({super.key, required this.message, required this.messageType});
-final String message;
-final MessageType messageType;
+  const Message({
+    Key? key,
+    required this.message,
+    required this.messageType,
+  }) : super(key: key);
+
+  final String message;
+  final MessageType messageType;
+
   @override
   Widget build(BuildContext context) {
-    return messageType==MessageType.text?Text(
-                  message,
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                ):CachedNetworkImage(imageUrl: message)
-    ;
+    // final AudioPlayer audioPlayer = AudioPlayer();
+    bool isPlaying = false;
+
+    return messageType == MessageType.text
+        ? Text(
+            message,
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          )
+        : messageType == MessageType.image
+            ? CachedNetworkImage(imageUrl: message)
+            : messageType == MessageType.audio
+                ? StatefulBuilder(builder: (context, setState) {
+                    return IconButton(
+                      constraints: const BoxConstraints(
+                        minWidth: 100,
+                      ),
+                      onPressed: () async {
+                        // if (isPlaying) {
+                        //   await audioPlayer.pause();
+                        //   setState(() {
+                        //     isPlaying = false;
+                        //   });
+                        // } else {
+                        //   await audioPlayer.play(UrlSource(message));
+                          setState(() {
+                            isPlaying = true;
+                          });
+                        
+                      },
+                      icon: Icon(
+                        isPlaying ? Icons.pause_circle : Icons.play_circle,
+                      ),
+                    );
+                  })
+                : const SizedBox.shrink(); // Return an empty widget for other message types
   }
 }
